37351e50c5b1a61743fa516f62c77ed2975b5fb8
buse tuba kocabıyık <btubakocabiyik@gmail.com>
2022-05-09 21:02:20 +0300

    Factory 2 DI

---
+++ 02_DI/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Product product = new Product(new FileLogger());
+product.logger.Log();
+
+
+// Dependency Inversion
+// Dependency Injection
+// IoC => inversion of control
+// IoC Container
+
+class Product{
+    public ILogger logger;
+
+    public Product(ILogger logger)
+    {
+        this.logger = logger;
+    }
+}
+
+interface ILogger
+{
+    void Log();
+}
+
+class FileLogger : ILogger
+{
+    public void Log()
+    {
+        throw new NotImplementedException();
+    }
+}
