c0f02e16fb14314e1f7f7a22f88e4d559685d0df
buse tuba kocabıyık <btubakocabiyik@gmail.com>
2022-05-09 17:35:22 +0300

    Singleton 1

---
+++ 01_Singleton/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using _01_Singleton;
+
+Singleton singleton1 = Singleton.GetInstance();
+Singleton singleton2 = Singleton.GetInstance();
+
+if (singleton1.Equals(singleton2))
+    Console.WriteLine("Equal");
+else
+    Console.WriteLine("Not Equal");
---
+++ 01_Singleton/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Singleton
+{
+    class Singleton
+    {
+        static Singleton _instance;
+
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetInstance()
+        {
+            if(_instance == null)
+                _instance = new Singleton();
+            return _instance;
+        }
+    }
+}
