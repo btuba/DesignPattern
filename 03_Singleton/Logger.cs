8e59d55b60a43cf32e162765462071ad68cdff71
buse tuba kocabıyık <btubakocabiyik@gmail.com>
2022-05-09 17:35:39 +0300

    Singleton 3

---
+++ 03_Singleton/Logger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Singleton
+{
+    internal class Logger
+    {
+        // idiom 
+
+        public static readonly Logger Instance = new Logger();
+        protected Logger()
+        {
+        }
+    }
+}
