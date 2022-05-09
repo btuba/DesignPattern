ec942d721667da77194c84b8ba9bf897f4ffbf79
buse tuba kocabıyık <btubakocabiyik@gmail.com>
2022-05-09 17:35:30 +0300

    Singleton 2

---
+++ 02_Singleton/Logger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Singleton
+{
+    internal class Logger
+    {
+        static Logger _logger;
+        static object _lock = new object();
+
+        protected Logger()
+        {
+
+        }
+        public static Logger GetLogger()
+        {
+            if (_logger == null)
+            {
+                lock (_lock)
+                {
+                    if ( _logger == null )
+                        _logger = new Logger();
+                }
+            }
+            return _logger;
+        }
+    }
+}
