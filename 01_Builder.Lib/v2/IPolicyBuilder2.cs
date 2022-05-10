1fb9de31ad91418f4623bdbffbe4a9379435bf51
buse tuba kocabıyık <btubakocabiyik@gmail.com>
2022-05-10 20:18:28 +0300

    Builder v2

---
+++ 01_Builder.Lib/v2/IPolicyBuilder2.cs
@@ -0,0 +1,17 @@
+﻿using _01_Builder.Lib.v1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Builder.Lib.v2
+{
+    public interface IPolicyBuilder2
+    {
+        Insurer BuildInsurer(string name);
+        Insured BuildInsured(string name);
+        List<Coverage> BuildCoverage(decimal price);
+        Policy BuildPolicy(string insuredName, string insurerName, decimal price);
+    }
+}
---
+++ 01_Builder.Lib/v2/PolicyBuilder2.cs
@@ -0,0 +1,40 @@
+﻿using _01_Builder.Lib.v1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Builder.Lib.v2
+{
+    public class PolicyBuilder2 : IPolicyBuilder2
+    {
+        public List<Coverage> BuildCoverage(decimal price)
+        {
+            return new List<Coverage>() 
+            { 
+                new Coverage() { Price = price } 
+            };   
+        }
+
+        public Insured BuildInsured(string name)
+        {
+            return new Insured() { Name = name };
+        }
+
+        public Insurer BuildInsurer(string name)
+        {
+            return new Insurer() { Name = name};
+        }
+
+        public Policy BuildPolicy(string insuredName, string insurerName, decimal price)
+        {
+            return new Policy() 
+            { 
+                Insured = BuildInsured(insuredName),
+                Insurer = BuildInsurer(insurerName),
+                Coverages = BuildCoverage(price)
+            };
+        }
+    }
+}
